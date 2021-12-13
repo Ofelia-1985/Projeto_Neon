cb265826f49a6789bcf90f775d0b5e8072b24f72
Carmen Ofelia Urbina Lopez <urbinalcarmen@gmail.com>
2021-12-13 13:05:22 -0300

    navegacao entre telas

---
+++ cadastro.js
@@ -0,0 +1,9 @@
+var cadastro = document.getElementById("cadastrar");
+         
+         cadastro.addEventListener("click", cadastrar);
+
+         function cadastrar() {
+
+            location.href = "http://127.0.0.1:5502/tela1.html";
+            
+         }
---
+++ script.js
@@ -0,0 +1,9 @@
+var primero = document.getElementById("btn1");
+         
+         primero.addEventListener("click", cadastrar);
+
+         function cadastrar() {
+
+            location.href = "http://127.0.0.1:5502/cadastro.html";
+            
+         }
