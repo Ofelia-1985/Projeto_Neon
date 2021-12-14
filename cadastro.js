2f488cc7bd961107a046325601b459ba9389e872
Moniquesouzas <monique.souzas@gmail.com>
2021-12-14 06:32:19 -0300

    Atualização dos arquivos na pasta

--- cadastro.js
+++
@@ -1,9 +0,0 @@
-var cadastro = document.getElementById("cadastrar");
-         
-         cadastro.addEventListener("click", cadastrar);
-
-         function cadastrar() {
-
-            location.href = "http://127.0.0.1:5502/tela1.html";
-            
-         }
--- script.js
+++
@@ -1,9 +0,0 @@
-var primero = document.getElementById("btn1");
-         
-         primero.addEventListener("click", cadastrar);
-
-         function cadastrar() {
-
-            location.href = "http://127.0.0.1:5502/cadastro.html";
-            
-         }
