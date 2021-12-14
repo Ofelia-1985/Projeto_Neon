73be7c903f8a7ad10e1b22d95079b5a7e87aef06
Carmen Ofelia Urbina Lopez <urbinalcarmen@gmail.com>
2021-12-14 12:31:51 -0300

    validacao Cadastro

---
+++ validacaoCadastro.js
@@ -0,0 +1,26 @@
+function validar (){
+
+    
+    var nome= formulario.nome.value;
+    var email= formulario.email.value;
+    var senha= formulario.senha.value;
+   
+
+    if (nome==""  ){
+        alert('Preencha corretamente o campo Nome.');
+        formulario.nome.focus();
+        return false;
+    }
+
+    if (email=="" || email.indexOf ('@')==-1){
+        alert('Preencha corretamente o campo Email.');
+        formulario.email.focus();
+        return false;
+    }
+
+    if (senha=="" || senha.length <=5){
+        alert('Preencha o campo Senha com mínimo 6 caracteres.');
+        formulario.senha.focus();
+        return false;
+    }
+}
