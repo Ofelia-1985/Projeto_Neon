edc97ec56db4f5a85f0fbc7d073e1c511f8a145b
Moniquesouzas <monique.souzas@gmail.com>
2021-12-15 18:27:55 -0300

    atualizei colunas da tabela e populei

---
+++ bancoDeDados/insert_login.sql
@@ -0,0 +1,8 @@
+
+USE projeto_neon;
+select * from login;
+insert into login (ID_CLIENTE, EMAIL, SENHA, NOME) value ('1', 'mariasantos@email.com', '12345678', 'Maria Santos');
+insert into login (ID_CLIENTE, EMAIL, SENHA, NOME) value ('2', 'rogerioalmeida@email.com', '12345678', 'Rogerio Almeida');
+insert into login (ID_CLIENTE, EMAIL, SENHA, NOME) value ('3', 'thiago.melo@email.com', '87654321', 'Thiago Melo');
+insert into login (ID_CLIENTE, EMAIL, SENHA, NOME) value ('4', 'helena_xavier@email.com', '123@4567', 'Helena Xavier');
+insert into login (ID_CLIENTE, EMAIL, SENHA, NOME) value ('5', 'Oliviadantas@email.com', '14253687', 'Olivia Dantas');
