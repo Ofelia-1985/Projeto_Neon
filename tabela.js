2d737f8b223d7deac92fd1f96a3de84e59e5537b
Carmen Ofelia Urbina Lopez <urbinalcarmen@gmail.com>
2021-12-16 03:36:26 -0300

    js tablela

---
+++ tabela.js
@@ -0,0 +1,9 @@
+var tablet = document.getElementById("lancar");
+         
+         tablet.addEventListener("click", lancamento);
+
+         function lancamento() {
+
+            location.href = "tela2.html";
+            
+         }
