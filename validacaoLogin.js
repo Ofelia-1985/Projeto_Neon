0cd6cb0f385d7112e545f72d207965309db4ca7a
Carmen Ofelia Urbina Lopez <urbinalcarmen@gmail.com>
2021-12-16 18:59:50 -0300

    nova validacao de login

--- validacaoLogin.js
+++ validacaoLogin.js
@@ -1,17 +1,39 @@
-function validar (){
+var primero = document.getElementById("btn1");
+         
+         primero.addEventListener("click", validar);
 
-    var email= formulario.email.value;
+var segundo = document.getElementById("btn2");
+
+        segundo.addEventListener("click", inscribirse);
+
+        function inscribirse(){
+
+            location.href = "cadastro.html";
+        };
+
+
+ function validar (){
+
+  
+ var email= formulario.email.value;
     var senha= formulario.senha.value;
 
-    if (email=="" || email.indexOf ('@')==-1){
-        alert('Preencha corretamente o campo Email.');
-        formulario.email.focus();
-        return false;
+    
+        if (email=="" || email.indexOf ('@')==-1){
+            alert('Preencha corretamente o campo Email.');
+            formulario.email.focus();
+            return false;
+        }
+    
+        if (senha=="" || senha.length <=5){
+            alert('Preencha o campo Senha com mínimo 6 caracteres.');
+            formulario.senha.focus();
+            return false;
+        
+         }else{
+    
+       
+             location.href = "tela1.html";
+            
+        }
     }
-
-    if (senha=="" || senha.length <=5){
-        alert('Preencha o campo Senha com mínimo 6 caracteres.');
-        formulario.senha.focus();
-        return false;
-    }
-}
