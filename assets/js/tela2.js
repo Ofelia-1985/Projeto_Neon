ae547c16157408e9d9841c1152ba1c9e7b7f9f3c
Carmen Ofelia Urbina Lopez <urbinalcarmen@gmail.com>
2021-12-17 03:04:25 -0300

    validacao da tela 2

--- assets/js/tela2.js
+++ assets/js/tela2.js
@@ -1,10 +1,38 @@
-var lancamento = document.getElementById("lancar");
+var primero = document.getElementById("btn1");
          
-         lancamento.addEventListener("click", lancar);
+         primero.addEventListener("click", validar);
 
-         function lancar() {
 
-            location.href = "tabela.html";
-            
-         }
-         
+
+function validar (){
+
+    
+    var data= formulario.data.value;
+    var valor= formulario.valor.value;
+    var descrip= formulario.descricao.value;
+   
+
+    if (data==""  ){
+        alert('Preencha corretamente o campo Data.');
+        formulario.data.focus();
+        return false;
+    }
+
+    if (valor=="" ||  valor.length >= 13) {
+        alert('Preencha corretamente o campo Valor até 12 caracteres.');
+        formulario.valor.focus();
+        return false;
+    }
+
+    if (descrip=="" || descricao.length >=51){
+        alert('Preencha o campo Descricão com máximo 50 caracteres.');
+        formulario.descricao.focus();
+        return false;
+    
+    }else{
+
+        location.href = "tabela.html";
+   
+    }
+
+}
