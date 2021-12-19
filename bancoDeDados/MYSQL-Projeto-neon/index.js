0d014ad7966a30e9da8e4f88711a961a38baeb46
Moniquesouzas <monique.souzas@gmail.com>
2021-12-19 05:19:53 -0300

    Criando rotas API

--- bancoDeDados/MYSQL-Projeto-neon/index.js
+++
@@ -1,7 +0,0 @@
-(async () => {
-    const db = require("./db");
-    console.log('Começou!');
-    console.log('SELECT * FROM login');
-    const login = await db.selectLogin();
-    console.log(login);
-}) ();
---
+++ tela2.js
@@ -0,0 +1,10 @@
+var lancamento = document.getElementById("lancar");
+         
+         lancamento.addEventListener("click", lancar);
+
+         function lancar() {
+
+            location.href = "http://127.0.0.1:5502/tela2.html";
+            
+         }
+         
