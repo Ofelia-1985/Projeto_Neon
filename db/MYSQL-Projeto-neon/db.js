2b6625fab4a3d7e6efc6fd4ccb516d9f2987c0c2
Moniquesouzas <monique.souzas@gmail.com>
2021-12-20 02:00:14 -0300

    Atualinhação do cídigo

--- db/MYSQL-Projeto-neon/db.js
+++ db/MYSQL-Projeto-neon/db.js
@@ -14,11 +14,29 @@ async function connect() {
 
 connect();
 
-async function selectLogin() {
+async function selectLogin(){
     const conn = await connect();
-    const [rows] = await conn.query('SELECT * FROM login;')
-    return await rows;
-
+    const [rows] = await conn.query('SELECT * FROM login;');
+    return rows;
 }
 
-module.exports = {selectLogin}
+async function insertLogin(login){
+    const conn = await connect();
+    const sql = 'INSERT INTO login(ID_CLIENTE, EMAIL, SENHA, NOME) VALUES (?,?,?,?);';
+    const values = [login.ID_CLIENTE, login.EMAIL, login.SENHA, login.NOME];
+    return await conn.query(sql, values);
+}
+
+async function updateLogin(ID_CLIENTE, LOGIN){
+    const conn = await connect();
+    const sql = 'UPDATE login SET ID_CLIENTE=?, EMAIL=?, SENHA=? NOME=? WHERE ID_CLIENTE=?';
+    const values = [ID_CLIENTE, login.EMAIL, login.SENHA, login.NOME];
+    return await conn.query(sql, values);
+}
+
+async function deleteLogin(ID_CLIENTE){
+    const conn = await connect();
+    const sql = 'DELETE FROM login WHERE ID_CLIENTE=?;';
+    return await conn.query(sql, [id]);
+}
+module.exports = {selectLogin, insertLogin, updateLogin, deleteLogin}
